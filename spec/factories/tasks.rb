c7df3f2d84acf8de4dcc02158e349538fcf8c880
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-20 12:06:30 -0700

    Creates factory for tasks
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :task do
+    name {Faker::Name.name}
+    description {Faker::Beer.style}
+    category {Faker::Food.description}
+    points {Faker::Number.within(range: 1..9999)}
+  end
+end
