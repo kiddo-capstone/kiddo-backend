657a2999f2da946153aacaf6503a43e3311b084f
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-20 12:15:32 -0700

    Adds points must be present
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ApplicationRecord
   has_many :mission_tasks
   has_many :missions, through: :mission_tasks
   validates :name, :description, :category, presence: true
-  validates :points, :numericality => { :greater_than => 0, only_integer: true }
+  validates :points, :numericality => { :greater_than => 0, only_integer: true }, presence: true
 
 end
