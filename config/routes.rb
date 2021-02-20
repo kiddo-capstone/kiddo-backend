e1cf53055249863ee0974be74ac8249fa22ad5a4
Will Dunlap <dunlapww@gmail.com>
2021-02-20 12:49:45 -0700

    add missions get route
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Shaun James <shaundanejames@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: :update
       resources :tasks
+      resources :missions, only: [:index]
     end
   end
 end
