70d702569fafd33950ed16a4742b464b240a0129
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-20 12:10:46 -0700

    Updates schema for new column name for tasks
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.string "type"
+    t.string "category"
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
