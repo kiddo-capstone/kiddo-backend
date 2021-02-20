3dbbb0bfa081274c560d40fcb5096b511c2e869c
Connor Ferguson <cpfergus1@gmail.com>
2021-02-19 17:59:30 -0700

    Implemented tests for user CRUD
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Mission, type: :model do
   describe 'relationships' do
     it {should have_many :mission_tasks}
-    it {should have_many :user_missions}
     it {should have_many(:tasks).through(:mission_tasks)}
-    it {should have_many(:users).through(:user_missions)}
+    it {should belong_to(:user)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "validations" do
-    it {should have_secure_password}
     it {should validate_presence_of :name}
-    it {should validate_uniqueness_of :name}
+    it {should validate_presence_of :email}
+    it {should validate_uniqueness_of :email}
   end
 
   describe 'relationships' do
-    it {should have_many :user_missions}
-    it {should have_many(:missions).through(:user_missions)}
+    it {should have_many(:missions)}
   end
 end
