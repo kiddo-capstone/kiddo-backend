0400c2247da4c37101eb91add0fc79a112953346
Will Dunlap <dunlapww@gmail.com>
2021-02-20 15:28:04 -0700

    Add destroy route for missions
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Shaun James <shaundanejames@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: :update
       resources :tasks
-      resources :missions, only: [:index, :create, :destroy]
+      resources :missions, only: [:index, :create, :destroy, :update]
     end
   end
 end
