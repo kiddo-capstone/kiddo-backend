a55dd8a367e00a6ed08c7ba19c6ea31b89cdc776
Will Dunlap <dunlapww@gmail.com>
2021-02-20 15:42:53 -0700

    add show route for missions
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Shaun James <shaundanejames@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: :update
       resources :tasks
-      resources :missions, only: [:index, :create, :destroy, :update]
+      resources :missions
     end
   end
 end
