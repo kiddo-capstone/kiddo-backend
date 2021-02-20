2a2d3996194082e636bd3a93f120ed757aeb3d07
Will Dunlap <dunlapww@gmail.com>
2021-02-20 14:16:26 -0700

    add create route to missions controller
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>
    Co-authored-by: Shaun James <shaundanejames@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, except: :update
       resources :tasks
-      resources :missions, only: [:index]
+      resources :missions, only: [:index, :create]
     end
   end
 end
