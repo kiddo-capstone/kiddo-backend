d41053bb358fb4a5b28fcbc7dc320b6866514123
Connor Ferguson <cpfergus1@gmail.com>
2021-02-19 18:05:21 -0700

    updated Models reflecting schema changes
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,6 @@
 class Mission < ApplicationRecord
-  has_many :user_missions
+  belongs_to :user
   has_many :mission_tasks
-  has_many :users, through: :user_missions
   has_many :tasks, through: :mission_tasks
   validates_presence_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :password, confirmation: {case_sensitive: true}, presence: true
-  validates :name, uniqueness: true, presence: true
-  has_many :user_missions
-  has_many :missions, through: :user_missions
+  has_many :missions
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
