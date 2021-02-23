eb273ba33a55a7c8f41d4a258b241b12c4f4f7a1
Will Dunlap <dunlapww@gmail.com>
2021-02-23 12:36:48 -0700

    add mission model test for get_list

--- spec/models/mission_spec.rb
+++ spec/models/mission_spec.rb
@@ -12,4 +12,23 @@ RSpec.describe Mission, type: :model do
     it {should validate_presence_of :due_date}
     it {should validate_presence_of :user_id}
   end
+
+  describe 'class methods' do
+    it 'get_tasks' do
+      mission = create(:mission)
+      task1, task2, task3, task4 = create_list(:task, 4)
+      mission.tasks << task1
+      mission.tasks << task2
+      mission.tasks << task3
+      mission.tasks << task4
+
+      expected = Mission.get_tasks(mission.id)
+      expected.each do |task|
+        expect(task).to be_a Task
+      end
+
+      expected = Mission.get_tasks(2343)
+      expect(expected).to be_nil
+    end
+  end
 end
