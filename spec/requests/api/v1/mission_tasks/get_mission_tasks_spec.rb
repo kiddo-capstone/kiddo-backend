29f8bf2d89ec4d950b82ceccefb7e42a95e7718f
Will Dunlap <dunlapww@gmail.com>
2021-02-23 12:38:22 -0700

    add request spec for get mission_tasks

--- spec/requests/api/v1/mission_tasks/get_mission_tasks_spec.rb
+++ spec/requests/api/v1/mission_tasks/get_mission_tasks_spec.rb
@@ -13,5 +13,28 @@ describe 'it can return a missions task with task details' do
     expect(response).to be_successful
 
     body = JSON.parse(response.body, symbolize_names: true)
+    expect(body).to have_key(:data)
+    task = body[:data].first
+    expect(task).to have_key(:id)
+    expect(task).to have_key(:type)
+    expect(task).to have_key(:attributes)
+    task_detail = task[:attributes]
+    expect(task_detail).to have_key(:task_id)
+    expect(task_detail).to have_key(:mission_id)
+    expect(task_detail).to have_key(:task_name)
+    expect(task_detail).to have_key(:task_description)
+    expect(task_detail).to have_key(:points)
+    expect(task_detail).to have_key(:task_category)
+    expect(task_detail).to have_key(:message)
+    expect(task_detail).to have_key(:image_path)
+    expect(task_detail).to have_key(:is_completed)
+  end
+
+  it 'returns an error json if mission id doesnt exist' do
+    get "/api/v1/missions/423/tasks"
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    expected = {:data=>{:errors=>"mission does not exist.", :status=>"bad_request"}}
+    expect(body).to eq(expected)
   end
 end
