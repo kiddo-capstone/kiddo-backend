a14130a64c7eb378cb5ec859a98240af86670967
Will Dunlap <dunlapww@gmail.com>
2021-02-22 19:07:17 -0700

    Clean up index action of MissionTasks controller

--- app/controllers/api/v1/mission_tasks_controller.rb
+++ app/controllers/api/v1/mission_tasks_controller.rb
@@ -1,25 +1,6 @@
 class Api::V1::MissionTasksController < ApplicationController
   def index
-    tasks = Mission.find(params[:id])
-                   .tasks
-                   .select("mission_tasks.id,
-                            mission_tasks.mission_id,
-                            mission_tasks.task_id,
-                            mission_tasks.message,
-                            mission_tasks.image_path,
-                            tasks.name as task_name,
-                            tasks.description as task_description,
-                            tasks.category as task_category,
-                            tasks.points")
-                            
-    mission_tasks = { data: [] }
-
-    tasks.map do |task|
-      task_hash = JSON.parse(task.to_json, symbolize_names: true)
-      mission_tasks[:data] << { id: task_hash[:id],
-                                type: 'mission_task',
-                                attributes: task_hash }
-    end
-    render json: mission_tasks
+    tasks = Mission.get_tasks(params[:id])
+    render json: MissionTaskListSerializer.new(tasks)
   end
 end
