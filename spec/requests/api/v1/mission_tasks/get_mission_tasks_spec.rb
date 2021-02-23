16b7c840006eab18cfc6f38af01cc7122ab6b032
Will Dunlap <dunlapww@gmail.com>
2021-02-22 19:07:55 -0700

    begin spec for mission_tasks

---
+++ spec/requests/api/v1/mission_tasks/get_mission_tasks_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'it can return a missions task with task details' do
+  it 'can return the tasks' do
+    mission = create(:mission)
+    task1, task2, task3, task4 = create_list(:task, 4)
+    mission.tasks << task1
+    mission.tasks << task2
+    mission.tasks << task3
+    mission.tasks << task4
+
+    get "/api/v1/missions/#{mission.id}/tasks"
+    expect(response).to be_successful
+
+    body = JSON.parse(response.body, symbolize_names: true)
+    require 'pry'; binding.pry
+  end
+end
