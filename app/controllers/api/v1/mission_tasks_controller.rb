a2d34cb88db7c1f3c24ac369c9b48d15f9b03181
Will Dunlap <dunlapww@gmail.com>
2021-02-22 22:30:12 -0700

    remove params method until needed

--- app/controllers/api/v1/mission_tasks_controller.rb
+++ app/controllers/api/v1/mission_tasks_controller.rb
@@ -3,10 +3,4 @@ class Api::V1::MissionTasksController < ApplicationController
     tasks = Mission.get_tasks(params[:id])
     render json: MissionTaskListSerializer.new(tasks)
   end
-
-  private
-
-  def mt_params
-    params.permit(:id, :mission_id, :task_id, :is_completed, :message, :image_path)
-  end
 end
