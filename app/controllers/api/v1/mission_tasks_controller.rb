9a71a92658443d53ce1d5b058415227ef9bce444
Brett Turing <bjsherman80@gmail.com>
2021-02-24 11:19:22 -0700

    create update action for mission task
    Co-authored-by: Will Dunlap <53274357+dunlapwv@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co_authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>

--- app/controllers/api/v1/mission_tasks_controller.rb
+++ app/controllers/api/v1/mission_tasks_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::MissionTasksController < ApplicationController
+
+ 
   def index
     tasks = Mission.get_tasks(params[:id])
 
@@ -9,4 +11,27 @@ class Api::V1::MissionTasksController < ApplicationController
       json_errors(errors, :bad_request)
     end
   end
+
+  def update 
+    mission_task = MissionTask.find_by(id: params[:id])
+    if mission_task 
+      mission_task.update(mission_task_params)
+      render json: { message: 'sucessfully updated mission task!' }
+    else 
+      render json: { data: { errors:
+        task.
+        errors.
+        full_messages.
+        to_sentence,
+        status: 400 } },
+        status: :bad_request
+    end
+  end 
+
+
+  private 
+
+  def mission_task_params
+    params.permit(:is_completed, :message, :mission_id, :task_id, :image_path)
+  end 
 end
