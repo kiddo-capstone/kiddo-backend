bf4834c2e5b34a6c653375e0ccfa7f6887015cd5
Brett Turing <bjsherman80@gmail.com>
2021-02-24 11:28:58 -0700

    complete update test
    Co-authored-by: Will Dunlap <53274357+dunlapwv@users.noreply.github.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co_authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>

--- spec/requests/api/v1/mission_tasks/update_mission_tasks_spec.rb
+++ spec/requests/api/v1/mission_tasks/update_mission_tasks_spec.rb
@@ -5,18 +5,16 @@ describe " mission tasks" do
        mission = create(:mission)
        task1, task2, task3, task4 = create_list(:task, 4)
        mission.tasks << task1
-       mission.tasks << task2
-       mission.tasks << task3
-       mission.tasks << task4
-       
-       mission_task_id = mission.mission_tasks[0].id
+       mission_task = mission.mission_tasks[0]
        body = {is_completed: true, message: "Im done!!"}
        headers = {"CONTENT_TYPE" => "application/json"}
-       patch "/api/v1/mission_tasks/#{mission_task_id}", headers: headers, params: body.to_json
-  
+       patch "/api/v1/mission_tasks/#{mission_task.id}", headers: headers, params: body.to_json
        expect(response).to be_successful
        json_body = JSON.parse(response.body, symbolize_names: true)
        expect(json_body).to have_key(:message)
        expect(json_body[:message]).to eq("sucessfully updated mission task!")
+       mission_task.reload
+       expect(mission_task.message).to eq("Im done!!")
+       expect(mission_task.is_completed).to eq(true)
     end
 end 
