8a601dd4f08d3acc0706f8a195d0cf39105e790c
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:59:36 -0700

    Updates user table with auth token
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 5) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
   end
 
   add_foreign_key "mission_tasks", "missions"
