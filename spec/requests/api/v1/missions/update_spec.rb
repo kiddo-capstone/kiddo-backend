15db40de232441f568eff13178e31a6d1df95982
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:53:38 -0700

    Updates test for API authentication
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/missions/update_spec.rb
+++ spec/requests/api/v1/missions/update_spec.rb
@@ -5,7 +5,17 @@ describe 'I can make a request to update a mission' do
     @user = create(:user)
     @mission = create(:mission)
     @user2 = create(:user)
+
+    @calvin = User.create(name: 'Calvin',
+                          email: 'Calvin@example.com',
+                          auth_token: SecureRandom.uuid)
+    @headers = { 'CONTENT_TYPE' => 'application/json'}
+    @body = {
+             email: @calvin.email,
+             auth_token: @calvin.auth_token
+            }
   end
+
   it 'can update a mission name, date, or user' do
     new_due_date = Date.new(2020, 5, 16)
     new_mission_name = 'Clean the house'
@@ -13,7 +23,9 @@ describe 'I can make a request to update a mission' do
     headers = { 'CONTENT_TYPE' => 'application/json' }
     request_body = { due_date: new_due_date,
                      name: new_mission_name,
-                     user_id: new_user_id }
+                     user_id: new_user_id,
+                     email: @calvin.email,
+                     auth_token: @calvin.auth_token }
 
     put "/api/v1/missions/#{@mission.id}", headers: headers, params: JSON.generate(request_body)
     body = JSON.parse(response.body, symbolize_names: true)
@@ -29,7 +41,9 @@ describe 'I can make a request to update a mission' do
     new_user_id = @user2.id
     headers = { 'CONTENT_TYPE' => 'application/json' }
     request_body = { due_date: new_due_date,
-                     user_id: new_user_id }
+                     user_id: new_user_id,
+                     email: @calvin.email,
+                     auth_token: @calvin.auth_token  }
 
     patch "/api/v1/missions/#{@mission.id}", headers: headers, params: JSON.generate(request_body)
     body = JSON.parse(response.body, symbolize_names: true)
@@ -45,7 +59,9 @@ describe 'I can make a request to update a mission' do
     new_user_id = "5"
     headers = { 'CONTENT_TYPE' => 'application/json' }
     request_body = { due_date: new_due_date,
-                     user_id: new_user_id }
+                     user_id: new_user_id,
+                     email: @calvin.email,
+                     auth_token: @calvin.auth_token  }
 
     patch "/api/v1/missions/#{@mission.id}", headers: headers, params: JSON.generate(request_body)
     body = JSON.parse(response.body, symbolize_names: true)
