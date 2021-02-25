9b4d5d1ed38a543a049a04c5e858adfe528eca55
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:50:50 -0700

    Updates test for API auth
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/mission_tasks/update_mission_tasks_spec.rb
+++ spec/requests/api/v1/mission_tasks/update_mission_tasks_spec.rb
@@ -1,12 +1,23 @@
 require 'rails_helper'
 
-describe " mission tasks" do 
-    it 'can update a mission task' do 
+describe " mission tasks" do
+  before(:each) do
+    @calvin = User.create(name: 'Calvin',
+                          email: 'Calvin@example.com',
+                          auth_token: SecureRandom.uuid)
+    @headers = { 'CONTENT_TYPE' => 'application/json'}
+    @body = {
+             email: @calvin.email,
+             auth_token: @calvin.auth_token
+            }
+  end
+
+    it 'can update a mission task' do
        mission = create(:mission)
        task1 = create(:task)
        mission.tasks << task1
        mission_task = mission.mission_tasks[0]
-       body = {is_completed: true, message: "Im done!!"}
+       body = {is_completed: true, message: "Im done!!", email: @calvin.email, auth_token: @calvin.auth_token}
        headers = {"CONTENT_TYPE" => "application/json"}
        patch "/api/v1/mission_tasks/#{mission_task.id}", headers: headers, params: body.to_json
        expect(response).to be_successful
@@ -18,12 +29,12 @@ describe " mission tasks" do
        expect(mission_task.is_completed).to eq(true)
     end
 
-    it 'gets error message when cant find mission task' do 
+    it 'gets error message when cant find mission task' do
        mission = create(:mission)
        task1 = create(:task)
        mission.tasks << task1
        mission_task = mission.mission_tasks[700]
-       body = {is_completed: true, message: 123123}
+       body = {is_completed: true, message: 123123, email: @calvin.email, auth_token: @calvin.auth_token}
        headers = {"CONTENT_TYPE" => "application/json"}
        patch "/api/v1/mission_tasks/700", headers: headers, params: body.to_json
        expect(response).to_not be_successful
@@ -32,4 +43,4 @@ describe " mission tasks" do
        expect(json_body[:data]).to have_key(:errors )
        expect(json_body[:data][:errors]).to eq("mission task does not exist.")
     end
-end 
+end
