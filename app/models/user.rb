6ddd42a4beb4bd48f63d9f3a874c08b789ac5ed4
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:55:24 -0700

    Updates user model to verify secure auth token
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,21 @@
 class User < ApplicationRecord
   has_many :missions
+  has_secure_token :auth_token
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+
+  before_create :set_auth_token
+
+  private
+
+  def set_auth_token
+    self.auth_token = generate_token
+  end
+
+  def generate_token
+    loop do
+      token = SecureRandom.uuid
+      break token unless User.where(auth_token: token).exists?
+    end
+  end
 end
