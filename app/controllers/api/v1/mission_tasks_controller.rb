13244ab84120672dd79052792deaaa9156ca26c7
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:57:35 -0700

    Updates mission task controller for auth check
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- app/controllers/api/v1/mission_tasks_controller.rb
+++ app/controllers/api/v1/mission_tasks_controller.rb
@@ -1,19 +1,19 @@
 class Api::V1::MissionTasksController < ApplicationController
-
+  before_action :auth_check
 
   def show
     json_create(MissionTask.find(params[:id]))
   end
 
-  def create 
+  def create
     mission_task = MissionTask.new(mission_task_params)
     if mission_task.save
       json_create(mission_task)
-    else 
+    else
       errors = mission_task.errors.full_messages.to_sentence
       json_errors(errors, :bad_request)
     end
-  end 
+  end
 
 
   def index
@@ -26,23 +26,23 @@ class Api::V1::MissionTasksController < ApplicationController
     end
   end
 
-  def update 
+  def update
     mission_task = MissionTask.find_by(id: params[:id])
-    if mission_task 
+    if mission_task
       mission_task.update(mission_task_params)
       render json: { message: 'sucessfully updated mission task!' }
-    else 
+    else
       errors = "mission task does not exist."
       json_errors(errors, :bad_request)
     end
-  end 
+  end
 
   def destroy
     MissionTask.destroy(params[:id])
-  end 
+  end
 
 
-  private 
+  private
 
   def json_create(mission_task)
     render json: MissionTaskSerializer.new(mission_task)
@@ -50,5 +50,5 @@ class Api::V1::MissionTasksController < ApplicationController
 
   def mission_task_params
     params.permit(:is_completed, :message, :mission_id, :task_id, :image_path)
-  end 
+  end
 end
