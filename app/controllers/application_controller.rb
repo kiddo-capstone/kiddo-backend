fd5fd4d574911f19115f683485fda1c18b4fd735
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:56:48 -0700

    Updates application controller with user model method for auth token
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,11 @@ private
     render json: { data: { errors: errors, status: response_status } }, status: response_status
   end
 
+  def auth_check
+    user_params = JSON.parse(request.body.read, symbolize_names: true)
+    user = User.find_by(email: user_params[:email])
+    unless user.auth_token == user_params[:auth_token]
+      render json: { message: 'unsuccessful', error: 'Missing Auth Token' }
+    end
+  end
 end
