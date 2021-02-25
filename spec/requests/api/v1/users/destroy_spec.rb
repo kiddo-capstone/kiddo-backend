dde18a3e1e4a62c45961c3b1a8a94b823544e282
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:54:36 -0700

    Updates test for API authentication
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/users/destroy_spec.rb
+++ spec/requests/api/v1/users/destroy_spec.rb
@@ -1,10 +1,20 @@
+require 'rails_helper'
+
 describe 'user destroy api' do
   it 'can destroy a user' do
-    user = create(:user)
+    calvin = User.create(name: 'Calvin',
+                         email: 'Calvin@example.com',
+                         auth_token: SecureRandom.uuid)
+
+    body = {
+             email: calvin.email,
+             auth_token: calvin.auth_token
+           }
+
     expect(User.count).to eq(1)
-    delete "/api/v1/users/#{user.id}"
+    delete "/api/v1/users/#{calvin.id}", headers: headers, params: JSON.generate(body)
     expect(response).to be_successful
     expect(User.count).to eq(0)
-    expect{ User.find(user.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    expect{ User.find(calvin.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
