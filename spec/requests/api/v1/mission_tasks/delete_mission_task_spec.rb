8dee0867ee129d27362edb5b5b3a0530adad1f52
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:49:19 -0700

    Updates test for API auth
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/mission_tasks/delete_mission_task_spec.rb
+++ spec/requests/api/v1/mission_tasks/delete_mission_task_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 describe 'mission_tasks' do
+  before(:each) do
+    @calvin = User.create(name: 'Calvin',
+                          email: 'Calvin@example.com',
+                          auth_token: SecureRandom.uuid)
+  end
+
   it 'can delete a mission task' do
     mission = create(:mission)
-    task1 = create(:task)
-    mission.tasks << task1
+    task_1 = create(:task)
+    mission.tasks << task_1
     mission_task = mission.mission_tasks.first
-    delete "/api/v1/mission_tasks/#{mission_task.id}"
+
+    body = { mission_id: mission.id, task_id: task_1.id, email: @calvin.email, auth_token: @calvin.auth_token }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    delete "/api/v1/mission_tasks/#{mission_task.id}", headers: headers, params: body.to_json
     expect(response).to be_successful
     expect(MissionTask.count).to eq(0)
     expect{ MissionTask.find(mission_task.id) }.to raise_error(ActiveRecord::RecordNotFound)
