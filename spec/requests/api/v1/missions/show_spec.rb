23fa4379e46a6ad4fcf9cf6b0bab8aeb18d910de
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:52:33 -0700

    Needs update for authorizing get HTTP verbs
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/missions/show_spec.rb
+++ spec/requests/api/v1/missions/show_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 describe 'we can see details of specific mission' do
-  it 'can send a json of a specific mission' do
+  before(:each) do 
+  @calvin = User.create(name: 'Calvin',
+                        email: 'Calvin@example.com',
+                        auth_token: SecureRandom.uuid)
+  @headers = { 'CONTENT_TYPE' => 'application/json'}
+  @body = {
+           email: @calvin.email,
+           auth_token: @calvin.auth_token
+          }
+end
+
+  xit 'can send a json of a specific mission' do
     user = create(:user)
     mission = create(:mission)
 
-    get "/api/v1/missions/#{mission.id}"
+    get "/api/v1/missions/#{mission.id}", headers: @headers, params: JSON.generate(@body)
     body = JSON.parse(response.body, symbolize_names: true)
     expect(body[:data][:id]).to eq(mission.id.to_s)
     expect(body[:data][:type]).to eq("mission")
@@ -16,9 +27,9 @@ describe 'we can see details of specific mission' do
     expect(body[:data][:attributes][:updated_at].to_date.strftime("%F")).to eq(mission.updated_at.to_date.strftime("%F"))
   end
 
-  it 'will send an error if mission cant be found' do
+  xit 'will send an error if mission cant be found' do
 
-    get "/api/v1/missions/3"
+    get "/api/v1/missions/3", headers: @headers, params: JSON.generate(@body)
     body = JSON.parse(response.body, symbolize_names: true)
     expected = {:data=>{:errors=>"Unable to find mission", :status=>"bad_request"}}
     expect(body).to eq(expected)
