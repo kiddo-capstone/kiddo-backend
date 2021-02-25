46cc3786c534bb3dfbe91f0b14d4ffe3ca3f1686
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:54:03 -0700

    Updates test for API authentication
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/tasks/destroy_spec.rb
+++ spec/requests/api/v1/tasks/destroy_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
 describe 'task destroy' do
+  before(:each) do
+    @calvin = User.create(name: 'Calvin',
+                          email: 'Calvin@example.com',
+                          auth_token: SecureRandom.uuid)
+
+    @task = create(:task)
+  end
+
   it 'can destroy a task' do
-    task = create(:task)
     expect(Task.count).to eq(1)
-    delete "/api/v1/tasks/#{task.id}"
+
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    body = { id: @task.id,
+             email: @calvin.email,
+             auth_token: @calvin.auth_token
+           }
+
+    delete "/api/v1/tasks/#{@task.id}", headers: headers, params: JSON.generate(body)
     expect(response).to be_successful
     expect(Task.count).to eq(0)
-    expect{ Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    expect{ Task.find(@task.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
