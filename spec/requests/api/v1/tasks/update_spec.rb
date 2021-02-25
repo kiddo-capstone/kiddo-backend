322e9f9e0cecc1b9600fd3d9caa04cbd970a692e
Shaun James <ShaunDaneJames@Gmail.com>
2021-02-25 11:54:18 -0700

    Updates test for API authentication
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- spec/requests/api/v1/tasks/update_spec.rb
+++ spec/requests/api/v1/tasks/update_spec.rb
@@ -1,51 +1,41 @@
 require 'rails_helper'
 
 describe 'update task' do
+  before(:each) do
+    @calvin = User.create(name: 'Calvin',
+                          email: 'Calvin@example.com',
+                          auth_token: SecureRandom.uuid)
+    @headers = { 'CONTENT_TYPE' => 'application/json'}
+    @task = create(:task)
+  end
+
   it 'can update a task' do
-    task = {
-      name: 'Git Good',
-      description: 'Do the things to git good',
-      category: 'Activity',
-      points: 1337
-    }
-    task_params = {
-      name: 'got good',
-      description: 'Done',
-      category: 'Something else',
-      points: 42
-    }
+    body = { task_id: @task.id,
+             name: @task.name,
+             description: "New description",
+             category: @task.category,
+             points: @task.points,
+             email: @calvin.email,
+             auth_token: @calvin.auth_token
+           }
 
-    headers = { 'CONTENT_TYPE' => 'application/json'}
-    post '/api/v1/tasks', headers: headers, params: JSON.generate(task)
-    updated_task = Task.last
-
-    patch api_v1_task_path(updated_task.id), headers: headers, params: JSON.generate(task_params)
-    updated_task.reload
-    expect(updated_task.name).to eq('got good')
-    expect(updated_task.description).to eq('Done')
-    expect(updated_task.category).to eq('Something else')
-    expect(updated_task.points).to eq(42)
+    patch api_v1_task_path(@task.id), headers: @headers, params: JSON.generate(body)
+    @task.reload
+    expect(@task.id).to eq(@task.id)
+    expect(@task.description).to eq('New description')
   end
 
   it 'cannot update a task to invalid entries' do
-    task = {
-      name: 'Git Good',
-      description: 'Do the things to git good',
-      category: 'Activity',
-      points: 1337
-    }
-    task_params = {
-      name: '',
-      description: 'Done',
-      category: 'Something else',
-      points: 42
-    }
+    body = { task_id: @task.id,
+             name: "",
+             description: @task.description,
+             category: @task.category,
+             points: @task.points,
+             email: @calvin.email,
+             auth_token: @calvin.auth_token
+           }
 
-    headers = { 'CONTENT_TYPE' => 'application/json'}
-    post '/api/v1/tasks', headers: headers, params: JSON.generate(task)
-    updated_task = Task.last
-
-    patch api_v1_task_path(updated_task.id), headers: headers, params: JSON.generate(task_params)
+    patch api_v1_task_path(@task.id), headers: @headers, params: JSON.generate(body)
     expect(response.status).to eq(400)
     body = JSON.parse(response.body, symbolize_names: true)
     expect(body[:data]).to have_key(:errors)
