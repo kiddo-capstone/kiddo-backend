69759ff0cb69c6b4dfc314be2709bd1070364059
Brett Turing <bjsherman80@gmail.com>
2021-02-25 16:38:05 -0700

    add invite action to send invitation job
    Co-authored-by: Shaun James <ShaunDaneJames@Gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :missions
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
+
+  def invite
+    SendNewUserInvitationJob.perform_later(id)
+  end
 end
