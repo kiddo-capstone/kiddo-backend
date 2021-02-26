7eacc88cafdb0f92e6198a15a2b259b529007d13
Will Dunlap <dunlapww@gmail.com>
2021-02-26 12:19:42 -0700

    update mission task controller to return updated mission_task

--- app/controllers/api/v1/mission_tasks_controller.rb
+++ app/controllers/api/v1/mission_tasks_controller.rb
@@ -1,68 +1,57 @@
-
 class Api::V1::MissionTasksController < ApplicationController
-  
   def show
     json_create(MissionTask.find(params[:id]))
   end
 
-  def create 
+  def create
     mission_task = MissionTask.new(mission_task_params)
     if mission_task.save
       json_create(mission_task)
-    else 
+    else
       errors = mission_task.errors.full_messages.to_sentence
       json_errors(errors, :bad_request)
     end
-  end 
-
+  end
 
   def index
     tasks = Mission.get_tasks(params[:id])
     if tasks
       render json: MissionTaskListSerializer.new(tasks)
     else
-      errors = "mission does not exist."
+      errors = 'mission does not exist.'
       json_errors(errors, :bad_request)
     end
   end
 
-  def update 
+  def update
     mission_task = MissionTask.find_by(id: params[:id])
-    if mission_task 
-      require 'pry'; binding.pry
+    if mission_task
       mission_task.update(mission_task_params)
-      
-      #the path of the uploaded file is:
-      url = "https://wills-bucket.s3-us-west-2.amazonaws.com/#{mission_task.image.key}"
 
-      #to retrieve the actual file and store locally:
-      client = Aws::S3::Client.new(region: 'us-west-2')
-      client.get_object({ bucket:'wills-bucket', key: mission_task.image.key }, target: 'test.png')
-      
-      render json: { message: 'sucessfully updated mission task!' }
-    else 
-      errors = "mission task does not exist."
+      url = mission_task.image.service_url
+      mission_task.update(image_path: url)
+      json_create(mission_task)
+    else
+      errors = 'mission task does not exist.'
       json_errors(errors, :bad_request)
     end
-  end 
+  end
 
   def destroy
     MissionTask.destroy(params[:id])
-  end 
-
-
-  private 
-
-  def json_create(mission_task)
-    render json: MissionTaskSerializer.new(mission_task)
   end
 
   private
 
-  
+  def json_create(mission_task)
+    json_create(mission_task)
+  end
+
   def mission_task_params
     params.permit(:is_completed, :message, :mission_id, :task_id, :image_path, :image)
   end
 
-  
+  def json_create(mission_task)
+    render json: MissionTaskSerializer.new(mission_task)
+  end
 end
