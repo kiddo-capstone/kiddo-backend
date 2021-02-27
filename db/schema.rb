3a9ed94b1652517acdbe45b8ecce0cc9400111f4
Will Dunlap <dunlapww@gmail.com>
2021-02-27 13:31:52 -0700

    add db migration for user points field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_164049) do
+ActiveRecord::Schema.define(version: 2021_02_27_202108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_164049) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "points"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
