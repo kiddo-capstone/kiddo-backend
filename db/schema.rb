ad0722a58fd2d72851182a42aebbd1367b96225c
Brett Turing <bjsherman80@gmail.com>
2021-03-01 13:10:33 -0700

    fix csv file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_202108) do
+ActiveRecord::Schema.define(version: 2021_03_01_200524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_02_27_202108) do
     t.integer "points"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "photo"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ mission_1 = Mission.create(name: 'Independence Day', due_date: Time.now + 1.day,
 mission_2 = Mission.create(name: "Doom's Day", due_date: Time.now - 1.day, user_id: user_1.id)
 mission_3 = Mission.create(name: 'Day of reckoning', due_date: Time.now + 1.year, user_id: user_2.id)
 
-task_1 = Task.create(name: 'EQ level up', description: 'Say something kind', category: 'EQ', points: 100)
-task_2 = Task.create(name: 'IQ level up', description: 'Conquer homework', category: 'IQ', points: 50)
-task_3 = Task.create(name: 'Special', description: 'Make your bed', category: 'Misc', points: 1337)
+task_1 = Task.create(name: 'EQ level up', description: 'Say something kind', category: 'EQ', points: 100, photo: false)
+task_2 = Task.create(name: 'IQ level up', description: 'Conquer homework', category: 'IQ', points: 50, photo: true)
+task_3 = Task.create(name: 'Special', description: 'Make your bed', category: 'Misc', points: 1337, photo: true)
 
 mission_task_11 = MissionTask.create(mission_id: mission_1.id, 
                                     task_id: task_1.id, 
