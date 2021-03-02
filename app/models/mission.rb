adc4e712c0f863fd66ab72f3b753df138b3253ad
Will Dunlap <dunlapww@gmail.com>
2021-03-02 13:33:37 -0700

    add dependent destroy to missions for mission_tasks

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,6 +1,6 @@
 class Mission < ApplicationRecord
   belongs_to :user
-  has_many :mission_tasks
+  has_many :mission_tasks, dependent: :destroy
   has_many :tasks, through: :mission_tasks
   validates_presence_of :name, :due_date, :user_id
 
