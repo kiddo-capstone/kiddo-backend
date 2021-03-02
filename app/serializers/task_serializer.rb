d45679c3475a22e8453a842a1fb5204c6a46127f
Connor Ferguson <cpfergus1@gmail.com>
2021-03-01 19:09:14 -0700

    updated seeds and serializer
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,5 +1,6 @@
 class TaskSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :name, :description, :category, :points, :photo
+  attributes :name, :description, :category, :points, :photo,
+             :resource_alt, :resource_link, :resource_type
 end
