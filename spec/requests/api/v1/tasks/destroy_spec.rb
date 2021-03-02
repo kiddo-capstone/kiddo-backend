0dc02ced7abf196e61e59007d5efc65c3f8657c4
Will Dunlap <dunlapww@gmail.com>
2021-03-02 13:23:43 -0700

    add test to destroy a task when attached to a mission_task

--- spec/requests/api/v1/tasks/destroy_spec.rb
+++ spec/requests/api/v1/tasks/destroy_spec.rb
@@ -9,4 +9,14 @@ describe 'task destroy' do
     expect(Task.count).to eq(0)
     expect{ Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
   end
+
+  it 'can destroy a task when its connected to mission_task' do
+    task = create(:task)
+    mission = create(:mission)
+    mission.tasks << task
+    delete "/api/v1/tasks/#{task.id}"
+    expect(response).to be_successful
+    expect(Task.count).to eq(0)
+    expect{ Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
