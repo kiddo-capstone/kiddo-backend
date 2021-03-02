8c92a6cc08a0445ef54c7945085aacef7b92b48f
Will Dunlap <dunlapww@gmail.com>
2021-03-02 13:24:15 -0700

    update task model for dependent: :destroy missiontasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ApplicationRecord
-  has_many :mission_tasks
+  has_many :mission_tasks, dependent: :destroy
   has_many :missions, through: :mission_tasks
   validates :name, :description, :category, presence: true
   validates :points, :numericality => { :greater_than => 0, only_integer: true }, presence: true
