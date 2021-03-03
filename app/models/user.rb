eb94caf8251c35bb254abdcf1a9d652178bb6422
Will Dunlap <dunlapww@gmail.com>
2021-03-03 09:06:35 -0700

    Add mission_tasks relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :missions, dependent: :destroy
+  has_many :mission_tasks, through: :missions
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   after_initialize :set_defaults
