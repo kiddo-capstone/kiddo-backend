d5815860de6c809e2b5d9ef2fbe0f1c010346f3c
Will Dunlap <dunlapww@gmail.com>
2021-03-12 15:14:13 -0700

    remove test.sql and facades

--- app/facades/user_stats_facade.rb
+++
@@ -1,9 +0,0 @@
-class UserStatsFacade
-  def self.get_user_stats(id)
-    stats = User.find_by(id: id).user_stats
-    new_stats = stats.map do |stat|
-      UserStat.new(stat)
-    end
-  end
-
-end
