aceb15f4b8a3f98976193a3809bf74b681592cfd
Will Dunlap <dunlapww@gmail.com>
2021-03-12 13:46:59 -0700

    clean up task factory

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :task do
     name {Faker::Name.name}
     description {Faker::Beer.style}
-    category {Faker::Food.description}
+    categories = ['brain training', 'health training', 'work training']
+    category {categories.sample}
     points {Faker::Number.within(range: 1..10)}
   end
 end
