369f8048d2a2383ab570301b013752a3742c31e4
Brett Turing <bjsherman80@gmail.com>
2021-03-16 15:15:30 -0600

    remove email from user model
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :missions, dependent: :destroy
-  validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   after_initialize :set_defaults
 end
