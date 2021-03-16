a5e3f525c056fb1057cf4be2a64565134ffff61f
Brett Turing <bjsherman80@gmail.com>
2021-03-16 15:15:58 -0600

    remove  email
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
+ActiveRecord::Schema.define(version: 12) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,13 @@ ActiveRecord::Schema.define(version: 9) do
     t.index ["user_id"], name: "index_missions_on_user_id"
   end
 
+  create_table "parents", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.string "description"
@@ -72,14 +79,16 @@ ActiveRecord::Schema.define(version: 9) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "points"
+    t.bigint "parent_id"
+    t.index ["parent_id"], name: "index_users_on_parent_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "mission_tasks", "missions"
   add_foreign_key "mission_tasks", "tasks"
   add_foreign_key "missions", "users"
+  add_foreign_key "users", "parents"
 end
