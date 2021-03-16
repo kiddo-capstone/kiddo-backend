9ce8f32b11ee4a950e689efdf4acc2c637eff6ec
Connor Ferguson <cpfergus1@gmail.com>
2021-03-16 17:22:34 -0600

    Found and corrected Typo
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- app/controllers/api/v1/parents_controller.rb
+++ app/controllers/api/v1/parents_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ParentsController < ApplicationController
     def index
-      json_create(Parnet.all)
+      json_create(Parent.all)
     end
-  
+
     def show
       json_create(Parent.find(params[:id]))
     end
-  
+
     def create
       if Parent.find_by(email: params[:email])
         json_create(Parent.find_by(email: params[:email]))
@@ -21,7 +21,7 @@ class Api::V1::ParentsController < ApplicationController
         end
       end
     end
-  
+
     def destroy
       parent = Parent.find_by(id: params[:id])
       if parent&.destroy
@@ -31,15 +31,14 @@ class Api::V1::ParentsController < ApplicationController
         json_errors(errors, :bad_request)
       end
     end
-  
+
     private
-  
+
     def json_create(task)
       render json: ParentSerializer.new(task)
     end
-  
+
     def parent_params
       params.permit(:name, :email, :updated_at, :created_at)
     end
   end
-  
