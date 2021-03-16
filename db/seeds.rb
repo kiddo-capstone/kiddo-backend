d552c8141f2a202c3cb24f704ed3309814788d1e
Connor Ferguson <cpfergus1@gmail.com>
2021-03-16 17:22:13 -0600

    Updated seeds
    Co-authored-by: Brett Sherman <bjsherman80@gmail.com>
    Co-authored-by: Shaun James <58994078+ShaunDaneJames@users.noreply.github.com>
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,15 @@ MissionTask.destroy_all
 Mission.destroy_all
 User.destroy_all
 Task.destroy_all
+Parent.destroy_all
 
+parent_1 = Parent.create(name: 'Calvin', email: 'Calvin@example.com')
+parent_2 = Parent.create(name: 'Hobbes', email: 'Hobbes@example.com')
 
-user_1 = User.create(name: 'Calvin', email: 'Calvin@example.com')
-user_2 = User.create(name: 'Hobbes', email: 'Hobbes@example.com')
+user_1 = User.create(name: 'Little Calvin', parent_id: parent_1.id, points: 123)
+user_2 = User.create(name: 'Little Joey', parent_id: parent_1.id, points: 333)
+user_3 = User.create(name: 'Little Hobbes', parent_id: parent_2.id, points: 111)
+user_4 = User.create(name: 'Little Sam', parent_id: parent_2.id, points: 421)
 
 mission_1 = Mission.create(name: 'Independence Day', due_date: Time.now + 1.day, user_id: user_1.id)
 mission_2 = Mission.create(name: "Doom's Day", due_date: Time.now - 1.day, user_id: user_1.id)
@@ -18,16 +23,16 @@ task_1 = Task.create(name: 'EQ level up', description: 'Say something kind', cat
 task_2 = Task.create(name: 'IQ level up', description: 'Conquer homework', category: 'IQ', points: 50, photo: true)
 task_3 = Task.create(name: 'Special', description: 'Make your bed', category: 'Misc', points: 1337, photo: true)
 
-mission_task_11 = MissionTask.create(mission_id: mission_1.id, 
-                                    task_id: task_1.id, 
+mission_task_11 = MissionTask.create(mission_id: mission_1.id,
+                                    task_id: task_1.id,
                                     message: "I'm Baaack!",
-                                    image_path: '', 
+                                    image_path: '',
                                     is_completed: false)
 
-mission_task_12 = MissionTask.create(mission_id: mission_1.id, 
-                                    task_id: task_2.id, 
+mission_task_12 = MissionTask.create(mission_id: mission_1.id,
+                                    task_id: task_2.id,
                                     message: 'Nailed it!',
-                                    image_path: '', 
+                                    image_path: '',
                                     is_completed: true)
 
 
@@ -36,7 +41,7 @@ mission_task_2 = MissionTask.create(mission_id: mission_2.id, task_id: task_2.id
 mission_task_3 = MissionTask.create(mission_id: mission_3.id, task_id: task_3.id, message: 'This too shall pass',
                                     image_path: '', is_completed: false)
 
-#csv stuff 
+#csv stuff
 CSV.foreach(Rails.root.join('db/data/tasks.csv'), headers: true) do |row|
  Task.create(row.to_h)
 end
