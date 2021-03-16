08a556e8d21298ca3d127917f2be7ce14af35008
Brett Turing <bjsherman80@gmail.com>
2021-03-16 15:18:12 -0600

    create parents crud spec
    Co-authored-by: Will Dunlap <dunlapww@gmail.com>
    Co-authored-by: Connor Ferguson <cpfergus1@gmail.com>

---
+++ spec/requests/parents/crud_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe 'parent create api' do
+  it 'create a parent' do
+    parent = {
+      name: 'Iamaboss',
+      email: 'Iamaboss@bossville.com'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/parents', headers: headers, params: JSON.generate(parent)
+    created_parent = Parent.last
+    expect(response).to be_successful
+    expect(created_parent.name).to eq(parent[:name])
+  end
+
+  it 'it will not create parent without email' do
+    parent = {
+      name: 'Iamaboss',
+      email: ''
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/parents', headers: headers, params: JSON.generate(parent)
+    expect(response.status).to eq(400)
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(body[:data]).to have_key(:errors)
+    expect(body[:data][:errors]).to be_a(String)
+  end
+  it 'it will not create parent without name' do
+    parent = {
+      name: '',
+      email: 'Iamaboss@bossville.com'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/parents', headers: headers, params: JSON.generate(parent)
+    expect(response.status).to eq(400)
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(body[:data]).to have_key(:errors)
+    expect(body[:data][:errors]).to be_a(String)
+  end
+  it 'it send back parent if parent exists' do
+    parent = {
+      name: 'bubba',
+      email: 'Iamaboss@bossville.com'
+    }
+    headers = { 'CONTENT_TYPE' => 'application/json'}
+    post '/api/v1/parents', headers: headers, params: JSON.generate(parent)
+    created_parent = Parent.last
+    expect(response).to be_successful
+    expect(created_parent.name).to eq(parent[:name])
+    expect(created_parent.email).to eq(parent[:email])
+
+    post '/api/v1/parents', headers: headers, params: JSON.generate(parent)
+    returned_parent = JSON.parse(response.body, symbolize_names: true)
+    expect(returned_parent[:data][:id]).to_not be_nil
+  end
+end
