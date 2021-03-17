031120f1b6bf87819ec8e8b5f3601792bd6ab7b5
Connor Ferguson <cpfergus1@gmail.com>
2021-03-17 16:00:29 -0600

    fixed user/parent relation issue in mission and factories

--- spec/factories/missions.rb
+++ spec/factories/missions.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :mission do
+    user
     name { "Weekly #{Faker::Creature::Animal.name} chores" }
     due_date { Time.zone.today + 5.days }
-    user
     created_at { Time.zone.today }
     updated_at { Time.zone.today }
   end
